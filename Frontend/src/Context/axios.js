9e919f8de9cf041f63cecb8fb96c1905e3ee97d8
rajnishchoudhary123 <rajnishc883@gmail.com>
2026-06-24 13:09:40 +0530

    most changes in the both backend and froentend

--- Frontend/src/Context/axios.js
+++ Frontend/src/Context/axios.js
@@ -1,9 +1,19 @@
 import axios from "axios";
 
-
-
-export default  axios.create({
-  baseURL: import.meta.env.VITE_SERVER,
-  withCredentials: true,
+const instance = axios.create({
+  baseURL: "http://localhost:5000",
+  
+  withCredentials: true
 });
 
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default instance;
