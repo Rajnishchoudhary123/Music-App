a472540340c8327c59f6eb1e648d49de22b601a4
rajnishchoudhary123 <rajnishc883@gmail.com>
2026-06-24 11:35:19 +0530

    comment the locallost origin in the server.js bakend file

--- Backend/server.js
+++ Backend/server.js
@@ -34,7 +34,7 @@ const PORT = process.env.PORT || 5000;
 server.use(
   cors({
     origin: [
-      "http://localhost:5173",
+    //   "http://localhost:5173",
       process.env.FRONTEND_URL,
     ],
     credentials: true,
