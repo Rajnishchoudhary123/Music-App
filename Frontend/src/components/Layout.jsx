c8d43a91a2ba7482e850371078b66df2ec02addc
rajnishchoudhary123 <rajnishc883@gmail.com>
2026-06-25 17:08:12 +0530

    changes in the backend paymentcontroller

--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -11,7 +11,7 @@ const Layout = ({ children }) => {
   return (
     <div className="h-screen bg-[#121212] text-white">
       <div className="h-[90%] flex relative">
-        {/* Mobile overlay */}
+     
         {sidebarOpen && (
           <div
             className="fixed inset-0 bg-black/50 z-[9998] lg:hidden"
@@ -19,7 +19,7 @@ const Layout = ({ children }) => {
           />
         )}
 
-        {/* Mobile Sidebar */}
+      
         <div
           className={`fixed inset-y-0 left-0 z-[9999] transform transition-transform duration-300 lg:hidden ${
             sidebarOpen ? "translate-x-0" : "-translate-x-full"
@@ -28,14 +28,13 @@ const Layout = ({ children }) => {
           <Sidebar mobile={true} closeSidebar={() => setSidebarOpen(false)} />
         </div>
 
-        {/* Desktop Sidebar */}
+       
         <div className="hidden lg:flex h-full">
           <Sidebar />
         </div>
 
-        {/* Main Content */}
         <div className="w-full m-2 px-4 sm:px-6 pt-4 pb-28 rounded bg-[#212121] overflow-auto lg:w-[75%] relative z-10">
-          {/* Mobile top bar */}
+         
           <div className="flex items-center justify-between mb-4 lg:hidden">
             <button
               onClick={() => setSidebarOpen(true)}
