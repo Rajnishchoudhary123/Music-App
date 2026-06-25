d13f5ee8847a4b18bff49d950607609ea59f5d6e
rajnishchoudhary123 <rajnishc883@gmail.com>
2026-06-25 12:15:08 +0530

    Changes in the server.js

--- Backend/server.js
+++ Backend/server.js
@@ -33,7 +33,7 @@ const PORT = process.env.PORT || 5000;
 
 server.use(
    cors({
-    origin: [process.env.FRONTEND_URL, process.env.VITE_SERVER, "http://localhost:5173"],
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
