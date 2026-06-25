412203b8cb85e2f011a85db83c7cfee21837fcef
rajnishchoudhary123 <rajnishc883@gmail.com>
2026-06-25 11:39:53 +0530

    Chnages in the axois file

--- Frontend/src/Context/axios.js
+++ Frontend/src/Context/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+
 export default axios.create({
-     baseURL: import.meta.env.VITE_API_URL,
+    
+  baseURL: baseURL,
+
   withCredentials: true
 });
