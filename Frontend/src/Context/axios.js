5e385ba8706da24e899e330c3f11370e96550679
rajnishchoudhary123 <rajnishc883@gmail.com>
2026-06-25 12:10:03 +0530

    Changes in the axios.js

--- Frontend/src/Context/axios.js
+++ Frontend/src/Context/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000";
-
+const baseURL = import.meta.env.VITE_SERVER
+console.log("Base URL:", baseURL);
 export default axios.create({
     
   baseURL: baseURL,
