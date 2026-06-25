6c4d8deaa37621f8cf4e2b0ff68a8cf9630b65d0
rajnishchoudhary123 <rajnishc883@gmail.com>
2026-06-25 10:22:07 +0530

    prepare backend for render deployment

--- Backend/server.js
+++ Backend/server.js
@@ -32,10 +32,8 @@ const server = express();
 const PORT = process.env.PORT || 5000;
 
 server.use(
-  cors({
-    origin: [
-      "http://localhost:5173"
-    ],
+   cors({
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
   })
 );
